76eba96e277b98424b12364b3cddb65170b4ee83
noinline <139589029+noinline@users.noreply.github.com>
2024-11-14 23:50:16 +0700

    undefined behavior

--- main.cpp
+++ main.cpp
@@ -22,145 +22,147 @@ teeHee(void) -> decltype(void())
   /* this function can be done in less lines but it looks ugly just like that,
    * and i also like how it looks so... cope + ratio + l + don't care + didn't
    * asked + stfu + you fell off + cry about it! */
-  const char *h[] = {"w", "w", "h", "e", "o"};
-  const char *w[] = {"l", "d", "r", "w", "o"};
-  const char *b[] = {">", "!", " ", "<", "w", "\n"};
-  asm volatile("movq $1, %%rax;"
-               "movq $1, %%rdi;"
-               "movq %0, %%rsi;"
-               "movq $1, %%rdx;"
-               "syscall;"
-               :
-               : "r"(h[2])
-               : "%rax", "%rdi", "%rsi", "%rdx");
-  asm volatile("movq $1, %%rax;"
-               "movq $1, %%rdi;"
-               "movq %0, %%rsi;"
-               "movq $1, %%rdx;"
-               "syscall;"
-               :
-               : "r"(h[3])
-               : "%rax", "%rdi", "%rsi", "%rdx");
-  asm volatile("movq $1, %%rax;"
-               "movq $1, %%rdi;"
-               "movq %0, %%rsi;"
-               "movq $1, %%rdx;"
-               "syscall;"
-               :
-               : "r"(h[0])
-               : "%rax", "%rdi", "%rsi", "%rdx");
-  asm volatile("movq $1, %%rax;"
-               "movq $1, %%rdi;"
-               "movq %0, %%rsi;"
-               "movq $1, %%rdx;"
-               "syscall;"
-               :
-               : "r"(h[1])
-               : "%rax", "%rdi", "%rsi", "%rdx");
-  asm volatile("movq $1, %%rax;"
-               "movq $1, %%rdi;"
-               "movq %0, %%rsi;"
-               "movq $1, %%rdx;"
-               "syscall;"
-               :
-               : "r"(h[4])
-               : "%rax", "%rdi", "%rsi", "%rdx");
-  asm volatile("movq $1, %%rax;"
-               "movq $1, %%rdi;"
-               "movq %0, %%rsi;"
-               "movq $1, %%rdx;"
-               "syscall;"
-               :
-               : "r"(b[2])
-               : "%rax", "%rdi", "%rsi", "%rdx");
-  asm volatile("movq $1, %%rax;"
-               "movq $1, %%rdi;"
-               "movq %0, %%rsi;"
-               "movq $1, %%rdx;"
-               "syscall;"
-               :
-               : "r"(w[3])
-               : "%rax", "%rdi", "%rsi", "%rdx");
-  asm volatile("movq $1, %%rax;"
-               "movq $1, %%rdi;"
-               "movq %0, %%rsi;"
-               "movq $1, %%rdx;"
-               "syscall;"
-               :
-               : "r"(w[4])
-               : "%rax", "%rdi", "%rsi", "%rdx");
-  asm volatile("movq $1, %%rax;"
-               "movq $1, %%rdi;"
-               "movq %0, %%rsi;"
-               "movq $1, %%rdx;"
-               "syscall;"
-               :
-               : "r"(w[2])
-               : "%rax", "%rdi", "%rsi", "%rdx");
-  asm volatile("movq $1, %%rax;"
-               "movq $1, %%rdi;"
-               "movq %0, %%rsi;"
-               "movq $1, %%rdx;"
-               "syscall;"
-               :
-               : "r"(w[0])
-               : "%rax", "%rdi", "%rsi", "%rdx");
-  asm volatile("movq $1, %%rax;"
-               "movq $1, %%rdi;"
-               "movq %0, %%rsi;"
-               "movq $1, %%rdx;"
-               "syscall;"
-               :
-               : "r"(w[1])
-               : "%rax", "%rdi", "%rsi", "%rdx");
-  asm volatile("movq $1, %%rax;"
-               "movq $1, %%rdi;"
-               "movq %0, %%rsi;"
-               "movq $1, %%rdx;"
-               "syscall;"
-               :
-               : "r"(b[1])
-               : "%rax", "%rdi", "%rsi", "%rdx");
-  asm volatile("movq $1, %%rax;"
-               "movq $1, %%rdi;"
-               "movq %0, %%rsi;"
-               "movq $1, %%rdx;"
-               "syscall;"
-               :
-               : "r"(b[2])
-               : "%rax", "%rdi", "%rsi", "%rdx");
-  asm volatile("movq $1, %%rax;"
-               "movq $1, %%rdi;"
-               "movq %0, %%rsi;"
-               "movq $1, %%rdx;"
-               "syscall;"
-               :
-               : "r"(b[0])
-               : "%rax", "%rdi", "%rsi", "%rdx");
-  asm volatile("movq $1, %%rax;"
-               "movq $1, %%rdi;"
-               "movq %0, %%rsi;"
-               "movq $1, %%rdx;"
-               "syscall;"
-               :
-               : "r"(b[4])
-               : "%rax", "%rdi", "%rsi", "%rdx");
-  asm volatile("movq $1, %%rax;"
-               "movq $1, %%rdi;"
-               "movq %0, %%rsi;"
-               "movq $1, %%rdx;"
-               "syscall;"
-               :
-               : "r"(b[3])
-               : "%rax", "%rdi", "%rsi", "%rdx");
-  asm volatile("movq $1, %%rax;"
-               "movq $1, %%rdi;"
-               "movq %0, %%rsi;"
-               "movq $1, %%rdx;"
-               "syscall;"
-               :
-               : "r"(b[5])
-               : "%rax", "%rdi", "%rsi", "%rdx");
+  return []() {
+    const char *h[] = {"w", "h", "e", "o"};
+    const char *w[] = {"l", "d", "r"};
+    const char *b[] = {">", "!", " ", "<", "\n"};
+    asm volatile("movq $1, %%rax;"
+                 "movq $1, %%rdi;"
+                 "movq %0, %%rsi;"
+                 "movq $1, %%rdx;"
+                 "syscall;"
+                 :
+                 : "r"(h[1])
+                 : "%rax", "%rdi", "%rsi", "%rdx");
+    asm volatile("movq $1, %%rax;"
+                 "movq $1, %%rdi;"
+                 "movq %0, %%rsi;"
+                 "movq $1, %%rdx;"
+                 "syscall;"
+                 :
+                 : "r"(h[2])
+                 : "%rax", "%rdi", "%rsi", "%rdx");
+    asm volatile("movq $1, %%rax;"
+                 "movq $1, %%rdi;"
+                 "movq %0, %%rsi;"
+                 "movq $1, %%rdx;"
+                 "syscall;"
+                 :
+                 : "r"(h[0])
+                 : "%rax", "%rdi", "%rsi", "%rdx");
+    asm volatile("movq $1, %%rax;"
+                 "movq $1, %%rdi;"
+                 "movq %0, %%rsi;"
+                 "movq $1, %%rdx;"
+                 "syscall;"
+                 :
+                 : "r"(h[0])
+                 : "%rax", "%rdi", "%rsi", "%rdx");
+    asm volatile("movq $1, %%rax;"
+                 "movq $1, %%rdi;"
+                 "movq %0, %%rsi;"
+                 "movq $1, %%rdx;"
+                 "syscall;"
+                 :
+                 : "r"(h[3])
+                 : "%rax", "%rdi", "%rsi", "%rdx");
+    asm volatile("movq $1, %%rax;"
+                 "movq $1, %%rdi;"
+                 "movq %0, %%rsi;"
+                 "movq $1, %%rdx;"
+                 "syscall;"
+                 :
+                 : "r"(b[2])
+                 : "%rax", "%rdi", "%rsi", "%rdx");
+    asm volatile("movq $1, %%rax;"
+                 "movq $1, %%rdi;"
+                 "movq %0, %%rsi;"
+                 "movq $1, %%rdx;"
+                 "syscall;"
+                 :
+                 : "r"(h[0])
+                 : "%rax", "%rdi", "%rsi", "%rdx");
+    asm volatile("movq $1, %%rax;"
+                 "movq $1, %%rdi;"
+                 "movq %0, %%rsi;"
+                 "movq $1, %%rdx;"
+                 "syscall;"
+                 :
+                 : "r"(h[3])
+                 : "%rax", "%rdi", "%rsi", "%rdx");
+    asm volatile("movq $1, %%rax;"
+                 "movq $1, %%rdi;"
+                 "movq %0, %%rsi;"
+                 "movq $1, %%rdx;"
+                 "syscall;"
+                 :
+                 : "r"(w[2])
+                 : "%rax", "%rdi", "%rsi", "%rdx");
+    asm volatile("movq $1, %%rax;"
+                 "movq $1, %%rdi;"
+                 "movq %0, %%rsi;"
+                 "movq $1, %%rdx;"
+                 "syscall;"
+                 :
+                 : "r"(w[0])
+                 : "%rax", "%rdi", "%rsi", "%rdx");
+    asm volatile("movq $1, %%rax;"
+                 "movq $1, %%rdi;"
+                 "movq %0, %%rsi;"
+                 "movq $1, %%rdx;"
+                 "syscall;"
+                 :
+                 : "r"(w[1])
+                 : "%rax", "%rdi", "%rsi", "%rdx");
+    asm volatile("movq $1, %%rax;"
+                 "movq $1, %%rdi;"
+                 "movq %0, %%rsi;"
+                 "movq $1, %%rdx;"
+                 "syscall;"
+                 :
+                 : "r"(b[1])
+                 : "%rax", "%rdi", "%rsi", "%rdx");
+    asm volatile("movq $1, %%rax;"
+                 "movq $1, %%rdi;"
+                 "movq %0, %%rsi;"
+                 "movq $1, %%rdx;"
+                 "syscall;"
+                 :
+                 : "r"(b[2])
+                 : "%rax", "%rdi", "%rsi", "%rdx");
+    asm volatile("movq $1, %%rax;"
+                 "movq $1, %%rdi;"
+                 "movq %0, %%rsi;"
+                 "movq $1, %%rdx;"
+                 "syscall;"
+                 :
+                 : "r"(b[0])
+                 : "%rax", "%rdi", "%rsi", "%rdx");
+    asm volatile("movq $1, %%rax;"
+                 "movq $1, %%rdi;"
+                 "movq %0, %%rsi;"
+                 "movq $1, %%rdx;"
+                 "syscall;"
+                 :
+                 : "r"(h[0])
+                 : "%rax", "%rdi", "%rsi", "%rdx");
+    asm volatile("movq $1, %%rax;"
+                 "movq $1, %%rdi;"
+                 "movq %0, %%rsi;"
+                 "movq $1, %%rdx;"
+                 "syscall;"
+                 :
+                 : "r"(b[3])
+                 : "%rax", "%rdi", "%rsi", "%rdx");
+    asm volatile("movq $1, %%rax;"
+                 "movq $1, %%rdi;"
+                 "movq %0, %%rsi;"
+                 "movq $1, %%rdx;"
+                 "syscall;"
+                 :
+                 : "r"(b[4])
+                 : "%rax", "%rdi", "%rsi", "%rdx");
+  }();
 }
 
 attribute_meowinline auto
